08fce524cd962ba9e8b462b0d88bd55f477a2f4f
lee <dbdks32123@gmail.com>
2024-04-17 19:04:53 +0900

    api hello 응답 방식 수정

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default function handler(req: NextRequest, res:NextResponse) {
-    res.status(200).json({ message: 'Hello, world!' });
+  return NextResponse.json({ message: 'Hello, world!' }, { status: 200 });
   }
