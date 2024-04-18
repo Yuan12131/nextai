feea9385135b7e32f833a5e5e12f7ad55fb8395b
lee <dbdks32123@gmail.com>
2024-04-18 17:48:37 +0900

    동적 라우트로 디렉토리 구조 변경

--- src/app/api/data/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  const { name } = req.query;
-  const greeting = `Hello, ${name || 'stranger'}!`;
-
-    return NextResponse.json({ greeting }, { status: 200 });
-  }
